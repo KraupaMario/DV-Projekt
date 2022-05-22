2211aa36afe7b16660540e70e4c930718754181b
rieggerd <dominik.raphael.riegger@hs-furtwangen.de>
2022-05-22 12:18:30 +0200

    Klasse Spiel mit einfacher Serververbindung
    Nullpointer Ex in Zeile 142

--- 2_Quellcode/Workspace/BlackJack/src/Spiel.java
+++ 2_Quellcode/Workspace/BlackJack/src/Spiel.java
@@ -1,8 +1,178 @@
+import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-public abstract class Spiel {
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+
+public class Spiel implements Runnable {
+
+	private String ip = "localhost";
+	private int port = 22222;
+	private Scanner scanner = new Scanner(System.in);
+	
+	private Thread thread;
+
+
+	private Socket socket;
+	private DataOutputStream dos;
+	private DataInputStream dis;
+
+	private ServerSocket serverSocket;
+
+
+
+	private String[] spaces = new String[9];
+
+	private boolean yourTurn = false; //bin ich an der Reihe?
+	private boolean client = true;	// Bin ich der Client?
+	private boolean host = false; // Bin ich der Host?
+	private boolean accepted = false;	//Bin ich schon mit einem Server verbunden?
+	private boolean unableToCommunicateWithOpponent = false; // Verbindung abgebrochen?
+	private boolean won = false; //ich habe den Spielzug gewonnen
+	private boolean dealerWon = false; // der Dealer hat gewonnen
+	
+
+
+	private int errors = 0;
+
+
+
+	private String waitingString = "Warten auf Mitspieler...";
+	private String unableToCommunicateWithOpponentString = "Kommunikation mit dem Spieler nicht möglich.";
+	private String wonString = "Du hast gewonnen!";
+	private String enemyWonString = "Dealer gewinnt!";
+	private String tieString = "Unentschieden!";
+
+	public Spiel() {
+		System.out.println("Bitte gib deine IP an: ");
+		ip = JOptionPane.showInputDialog("IP Adresse");
+		System.out.println("Bitte gib einen Port an: ");
+		port = Integer.parseInt(JOptionPane.showInputDialog("Port?"));
+		while (port < 1 || port > 65535) {
+			System.out.println("Dein Port war ungültig, bitte gib einen neuen ein: ");
+			port = Integer.parseInt(JOptionPane.showInputDialog("Port?"));
+		}
+
+		if (!verbunden())
+			initialisiereServer();
+		
+		thread = new Thread(this, "BlackJack");
+		thread.start();
+	}
+	
+	public void run() {
+		while (true) {
+			
+			//Programmcode welcher im "Thread" ausgeführt wird.
+			aktion();
+			if (!host && !accepted) {
+				wartenAufServer();
+			}
+
+		}
+	}
+
+	private void wartenAufServer() {
+		Socket socket = null;
+		try {
+			socket = serverSocket.accept();
+			dos = new DataOutputStream(socket.getOutputStream());
+			dis = new DataInputStream(socket.getInputStream());
+			accepted = true;
+			System.out.println("CLIENT HAS REQUESTED TO JOIN, AND WE HAVE ACCEPTED");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean verbunden() {
+		try {
+			socket = new Socket(ip, port);
+			dos = new DataOutputStream(socket.getOutputStream());
+			dis = new DataInputStream(socket.getInputStream());
+			accepted = true;
+		} catch (IOException e) {
+			System.out.println("Meine IP Adresse und Port: " + ip + " : " + port + " | Eröffne einen Server.");
+			return false;
+		}
+		System.out.println("Erfolgreich mit dem Server verbunden.");
+		return true;
+	}
+
+	private void initialisiereServer() {
+		try {
+			serverSocket = new ServerSocket(port, 8, InetAddress.getByName(ip));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		yourTurn = true;
+		host = true;
+		client = false;
+	}
+	
+	private void aktion()  {
+		String nachricht = "leer";
+		if (host && yourTurn) {
+			
+			/*
+			try {
+				String nachricht = dis.readUTF();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println("nachricht");
+			}
+			*/
+			
+			
+			System.out.println("Ich bin der Host und bin an der Reihe. Gib eine Nachricht ein.");
+			int txt1 = Integer.parseInt(JOptionPane.showInputDialog("Nachricht (Ich bin der Host)"));
+			try {
+				dos.writeInt(txt1);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			yourTurn = false;
+
+		} else if (!host && yourTurn) {
+			
+			try {
+				nachricht = dis.readUTF();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println(nachricht);
+			
+			System.out.println("Ich bin der Client und bin an der Reihe. Gib eine Nachricht ein.");
+			int txt2 = Integer.parseInt(JOptionPane.showInputDialog("Nachricht (Ich bin der Client)"));
+			try {
+				dos.writeInt(txt2);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			yourTurn = false;
+		}
+
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Spiel neu = new Spiel();
 
 	}
 
