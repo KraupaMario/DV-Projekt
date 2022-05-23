123f0da471335cf283ff1b32329d0cc6d3361352
Paule77 <102792527+Paule77@users.noreply.github.com>
2022-05-23 19:09:45 +0200

    Methoden für Datenaustausch

--- 2_Quellcode/Workspace/BlackJack/src/DataExchange.java
+++ 2_Quellcode/Workspace/BlackJack/src/DataExchange.java
@@ -1,9 +1,39 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.*;
+import javax.swing.*;
 
-public class DataExchange {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class DataExchange implements Serializable {
+	InetAddress addre;
+	String partner = JOptionPane.showInputDialog(null, "Wer ist Server ?");
+	Socket socket = null;
+	public static void main(String[] args) { 
+		
+		
+		
 	}
-
+	
+public void startServer() {
+	//Erstellung Socket
+	//Hoast und IP rauslessen
+	try {
+		addre= InetAddress.getByName(partner); //Partner Adresse besorgen
+		System.out.println("Ich bin" + InetAddress.getLocalHost());
+		System.out.println("Partner: " + addre);
+		socket = new Socket(addre, 6000);
+		System.out.println("socket = " + socket);
+	}
+	
+	catch (Exception e) {
+		System.out.println(e);}
 }
+
+public void serverRun() {
+	//mit accept verbinden	
+	
+	}
+}
+
