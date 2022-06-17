0496c42df30c07c30a2f61f621205b9c4e5a761f
rieggerd <dominik.raphael.riegger@hs-furtwangen.de>
2022-06-17 11:20:02 +0200

    Skelett Spielablauf programmiert #3 (Client)
    Weiterführen des Skeletts (Raw Spielablauf) und ergänzen weiterer Eventualitäten.
    
    Co-Authored-By: rieggerd <102793058+rieggerd@users.noreply.github.com>

--- 2_Quellcode/Workspace/BlackJack_Alpha/src/Game/Client2.java
+++ 2_Quellcode/Workspace/BlackJack_Alpha/src/Game/Client2.java
@@ -240,7 +240,7 @@ public class Client2 implements Runnable {
 			kartenausgebenS_R1(client);
 			kartenwertanzeigen(client);
 
-			/** BlackJack/Überkauft abfrage empfangen.*/
+			/** BlackJack/Überkauft Abfrage empfangen.*/
 			try {
 				client.winSpieler1 = dis.readBoolean();
 			} catch (IOException e1) {
@@ -259,10 +259,28 @@ public class Client2 implements Runnable {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+			try {
+				client.loseSpieler1 = dis.readBoolean();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				client.loseSpieler2 = dis.readBoolean();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				client.loseDealer = dis.readBoolean();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 
 
 			//Wenn BJ = true, mach neues Spiel (gehe zu newgame)
-			while((client.winSpieler1 || client.winSpieler2 || client.winDealer)) {
+			while((client.winSpieler1 || client.winSpieler2 || client.winDealer || client.loseSpieler1 || client.loseSpieler2 || client.loseDealer)) {
 				/** Auswertestatus der Spieler empfangen*/
 				try {
 					auswertStatSp1 = dis.readInt();
@@ -314,137 +332,93 @@ public class Client2 implements Runnable {
 				try {
 					dos.writeBoolean(hit1);
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace(); }
 				
 
 				while((!hit1 || !hit2)) {
-					System.out.println("Auswerten");
+					/**Auswertestatus der Spieler empfangen*/
+					try {
+						auswertStatSp1 = dis.readInt();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						auswertStatSp2 = dis.readInt();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					/** Gewinner/Verlierernachricht ausgeben*/
+					gewinnbenachrichtung();		
 					continue newgame;
 				}
 
 				while((hit1 && hit2)) {
-					System.out.println("Auswerten");
-					if (win) {
+					/**Karten vom Server empfangen.*/
+					//Karte für Spieler 1 empfangen:
+					client.DeckSpieler1.add(karteEmpfangen());
+					//Karte für Spieler 2 empfangen:
+					client.DeckSpieler2.add(karteEmpfangen());
+					//Karten für Dealer empfangen, sofern der Kartenwert unter 17 liegt:
+					if (client.wertDealer < 17) {
+						client.DeckDealer.add(karteEmpfangen());
+					}	
+					kartenausgebenS_R234(client, z);
+					kartenwertanzeigen(client);
+					
+					/** BlackJack/Überkauft Abfrage empfangen.*/
+					try {
+						client.winSpieler1 = dis.readBoolean();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						client.winSpieler2 = dis.readBoolean();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						client.winDealer = dis.readBoolean();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						client.loseSpieler1 = dis.readBoolean();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						client.loseSpieler2 = dis.readBoolean();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						client.loseDealer = dis.readBoolean();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					if ((client.winSpieler1 || client.winSpieler2 || client.winDealer || client.winDealer || client.loseSpieler1 || client.loseSpieler2 || client.loseDealer)) {
 						System.out.println("Ein spieler Gewinnt oder Überkauft sich");
 						break newcard;
 					}
 					continue newcard;
 				}
+			
 			}
-
-			a = true;
-			//continue newgame;
+			continue newgame;
 		}
-
-
-
-
-
-
-		//--------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-		/**Empfängt den Status, ob direkt nach dem austeilen ein Spieler verloren oder gewonnen hat*/
-		/**Status für Spieler 1:*/
-		try {
-			client.winSpieler1 = dis.readBoolean();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			client.loseSpieler1 = dis.readBoolean();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		/**Status für Spieler 2:*/
-		try {
-			client.winSpieler2 = dis.readBoolean();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			client.loseSpieler2 = dis.readBoolean();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		/**Status für Dealer*/
-		try {
-			client.winDealer = dis.readBoolean();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			client.loseDealer = dis.readBoolean();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		/**Spieler Status Spieler 1*/
-		if(client.winSpieler1 = true) {
-			ausgabetextS1C = "Sieger"; 
-			System.out.println("Sieger Spieler 1");
-			//Stop
-		}
-
-
-		if (client.loseSpieler1=true) {
-			ausgabetextS1C = "Verlierer"; 
-			System.out.println("Verlierer Spieler 1");
-			//Stop
-		}
-
-
-		/**Spieler Status Spieler 2*/
-		if(client.winSpieler2 = true) {
-			ausgabetextS2C = "Sieger";
-			System.out.println("Sieger Spieler 2");
-			//Stop
-		}
-
-
-		if (client.loseSpieler2=true) {
-			ausgabetextS2C = "Verlierer";
-			System.out.println("Verlierer Spieler 2");
-			//Stop
-		}
-
-
-		/**Spieler Status Dealer*/
-		if(client.winDealer = true) {
-			System.out.println("Sieger Spieler Dealer");
-			//Stop
-		}
-
-
-		if (client.loseDealer=true) {
-			System.out.println("Verlierer Spieler Dealer");
-			//Stop
-		}
-
-
-
-
-		System.out.println(client.DeckSpieler1.get(0).getFarbe());
-		System.out.println(client.DeckSpieler1.get(0).getName());
-		System.out.println(client.DeckSpieler1.get(1).getFarbe());
-		System.out.println(client.DeckSpieler1.get(1).getName());
-		kartenausgebenS_R1(client);//Karten anzeigen
-		kartenwertanzeigen(client);
-
-		rundeZuAuswerten();
-
-		thread.stop();
+	
 	}
+
+
 
 	void gewinnbenachrichtung ()
 
