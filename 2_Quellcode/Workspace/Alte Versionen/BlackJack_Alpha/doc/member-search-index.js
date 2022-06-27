5179a286976d9e688fbcd1c91d3c425d4733609c
rieggerd <dominik.raphael.riegger@hs-furtwangen.de>
2022-06-27 19:49:16 +0200

    JavaDoc + Alte Versionen Entfernt
    zur Besseren Übersicht.
    Alte Spiel-Versionen aus Workspace entfernt.
    Letzte Version: Black Jack Beta.
    +
    JavaDoc Speicherort richtiggestellt.

--- 2_Quellcode/Workspace/Alte Versionen/BlackJack_Alpha/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"Game","c":"Spieler","l":"abbuchen(int)"},{"p":"Game","c":"Spieler","l":"geldsetzen()"},{"p":"Game","c":"Spieler","l":"getKontostand()"},{"p":"Game","c":"Spieler","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"Game","c":"Spieler","l":"Spieler(String, String)","u":"%3Cinit%3E(java.lang.String,java.lang.String)"}];updateSearchResults();
--- 2_Quellcode/Workspace/_pasted_code_/src/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		C:\Users\Jannik\Documents\GitHub\DV-Projekt\2_Quellcode\Workspace\GUI\Bilder
-	}
-}
-
